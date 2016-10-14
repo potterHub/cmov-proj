27a625d0754f4a8bd1c8d5582778a13fd2350013
Hugo Drumond <hugo.drumond@fe.up.pt>
2016-10-14 01:06:29 +0100

    Found a good library that handles sessions. Ditched my code.

--- trabalho1/Server/src/server/main.go
+++ trabalho1/Server/src/server/main.go
@@ -2,15 +2,22 @@ package main
 
 import (
 	"github.com/pressly/chi"
+	"github.com/alexedwards/scs/engine/memstore"
+	"github.com/alexedwards/scs/session"
 	"server/users"
 	"server/terminals"
 	"server/items"
+	"net/http"
 )
 
 func main() {
 	router := chi.NewRouter()
 
+	engine := memstore.New(0)
+	sessionManager := session.Manage(engine)
+
 	router.Route(users.MainPath, users.SubRoutes)
 	router.Route(terminals.MainPath, terminals.SubRoutes)
 	router.Route(items.MainPath, items.SubRoutes)
+	http.ListenAndServe(":80", sessionManager(router))
 }

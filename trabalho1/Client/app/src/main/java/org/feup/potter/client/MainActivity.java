c53d2c6ec785f6bfa70d880b35e1bdf92ad935c3
potterHub <mouraviny20004@hotmail.com>
2016-10-23 02:48:10 +0100

    client application main menu layout and structure

--- trabalho1/Client/app/src/main/java/org/feup/potter/client/MainActivity.java
+++ trabalho1/Client/app/src/main/java/org/feup/potter/client/MainActivity.java
@@ -1,29 +1,66 @@
 package org.feup.potter.client;
 
+import java.util.ArrayList;
+
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.GridView;
+import android.widget.Toast;
 
-import org.feup.potter.client.log_in.SingUp;
+import org.feup.potter.client.main_menu.GridViewAdapter;
+import org.feup.potter.client.main_menu.Item;
 
-
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnItemClickListener
+{
+    GridView gridview;
+    GridViewAdapter gridviewAdapter;
+    ArrayList<Item> data = new ArrayList<Item>();
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        // starting login tab
-        // do something when clicking the button
-
-        // just get user from db (if exists log in)
-
-        // if not register fragment
-        Intent intent = new Intent(this,SingUp.class);
-
-        // starts the second activity
-        startActivity(intent);
+        initView(); // Initialize the GUI Components
+        fillData(); // Insert The Data
+        setDataAdapter(); // Set the Data Adapter
     }
+
+    // Initialize the GUI Components
+    private void initView()
+    {
+        gridview = (GridView) findViewById(R.id.gridView);
+        gridview.setOnItemClickListener(this);
+    }
+
+    // Insert The Data
+    private void fillData()
+    {
+        data.add(new Item("op 1", getResources().getDrawable(R.drawable.rest_img)));
+        data.add(new Item("op 2", getResources().getDrawable(R.drawable.rest_img)));
+        data.add(new Item("op 3", getResources().getDrawable(R.drawable.rest_img)));
+        data.add(new Item("op 4", getResources().getDrawable(R.drawable.rest_img)));
+        data.add(new Item("op 5", getResources().getDrawable(R.drawable.rest_img)));
+        data.add(new Item("op 6", getResources().getDrawable(R.drawable.rest_img)));
+    }
+
+    // Set the Data Adapter
+    private void setDataAdapter()
+    {
+        gridviewAdapter = new GridViewAdapter(getApplicationContext(), R.layout.grid_view_menu_icon, data);
+        gridview.setAdapter(gridviewAdapter);
+    }
+
+    @Override
+    public void onItemClick(final AdapterView<?> arg0, final View view, final int position, final long id)
+    {
+        String message = "Clicked : " + data.get(position).getTitle();
+        Toast.makeText(getApplicationContext(), message , Toast.LENGTH_SHORT).show();
+    }
+
 }
+

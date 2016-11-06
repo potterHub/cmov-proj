2619a157feb8701ba84e2c4ceb221614e57748fc
potterHub <mouraviny20004@hotmail.com>
2016-11-06 21:56:57 +0000

    update

--- trabalho1/Backend/server/src/server/models/sale.go
+++
@@ -1,9 +0,0 @@
-package models
-
-type Sale struct {
-	IdSale     int64      `json:"idSale"`
-	IdCustomer int64      `json:"idCustomer,omitempty"`
-	MyDateTime string     `json:"myDateTime"`
-	Items      []*SaleItem `json:"items"`
-	Vouchers   []*Voucher `json:"vouchers"`
-}
--- trabalho1/Client/app/src/main/java/org/feup/potter/client/db/VouchersInList.java
+++
@@ -1,7 +0,0 @@
-package org.feup.potter.client.db;
-
-import java.io.Serializable;
-
-public class VouchersInList implements Serializable {
-
-}
